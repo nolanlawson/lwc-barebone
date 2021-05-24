88134c4d4db59702008522757235409efc391406
Nolan Lawson <nlawson@salesforce.com>
2021-05-24 15:57:05 -0700

    try to repro highcharts issue

--- src/modules/x/app/app.js
+++ src/modules/x/app/app.js
@@ -1,3 +1,30 @@
 import { LightningElement } from 'lwc';
 
-export default class App extends LightningElement {}
+export default class App extends LightningElement {
+  renderedCallback() {
+    const container = this.template.querySelector('.container')
+    const chart = Highcharts.chart(container, {
+      chart: {
+        type: 'bar'
+      },
+      title: {
+        text: 'Fruit Consumption'
+      },
+      xAxis: {
+        categories: ['Apples', 'Bananas', 'Oranges']
+      },
+      yAxis: {
+        title: {
+          text: 'Fruit eaten'
+        }
+      },
+      series: [{
+        name: 'Jane',
+        data: [1, 0, 4]
+      }, {
+        name: 'John',
+        data: [5, 7, 3]
+      }]
+    });
+  }
+}
