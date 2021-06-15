019e79efb5a615f783a366842273df75a5757e99
Nolan Lawson <nlawson@salesforce.com>
2021-06-15 13:34:38 -0700

    light dom demo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createElement } from "lwc";
 import App from "x/app";
 
+lwcRuntimeFlags.ENABLE_LIGHT_DOM_COMPONENTS = true
+
 const elm = createElement("x-app", { is: App });
 document.body.appendChild(elm);
+
+const div = document.createElement('div')
+div.textContent = 'other div'
+document.body.appendChild(div)
