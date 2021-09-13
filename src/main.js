58eb833af335a35b51c118a7cb15c601efd48ef1
Nolan Lawson <nlawson@salesforce.com>
2021-09-13 13:01:29 -0700

    fix: repro elementsFromPoint issue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
+import "@lwc/synthetic-shadow"
 import { createElement } from "lwc";
 import App from "x/app";
 
 const elm = createElement("x-app", { is: App });
 document.body.appendChild(elm);
+
+const component = document.querySelector('x-app').shadowRoot.querySelector('x-component')
+const [ rect ] = component.getClientRects()
+const x = rect.x + (rect.width / 2)
+const y = rect.y + (rect.height / 2)
+const elements = document.elementsFromPoint(x, y)
+
+console.log('elementsFromPoint', elements)
