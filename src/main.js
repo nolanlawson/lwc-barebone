bd2013f59a7e0bbb8b91d759a208557de0f0fb08
Nolan Lawson <nlawson@salesforce.com>
2021-10-06 13:49:34 -0700

    serialization repro

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
+import '@lwc/synthetic-shadow'
 import { createElement } from "lwc";
 import App from "x/app";
 
+const div = document.createElement('div')
+document.body.appendChild(div);
+
 const elm = createElement("x-app", { is: App });
-document.body.appendChild(elm);
+div.appendChild(elm)
+
+console.log('div.innerHTML', div.innerHTML)
+console.log('div.outerHTML', div.outerHTML)
+console.log('div.textContent', div.textContent)
