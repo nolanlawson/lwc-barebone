431bec0c1e96197cf5847b8a3ccbb46ebd219515
Nolan Lawson <nlawson@salesforce.com>
2021-12-16 11:46:25 -0800

    fix: minimal repro for stylesheet bug

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createElement } from "lwc";
 import App from "x/app";
 
-const elm = createElement("x-app", { is: App });
-document.body.appendChild(elm);
+document.body.appendChild(createElement("x-app", { is: App }));
+
+const template = document.createElement('template')
+template.content.appendChild(createElement("x-app", { is: App }))
