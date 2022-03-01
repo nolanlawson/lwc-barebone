c365e18e69b0120943a7bea4701a99cd9e7e2fcb
Nolan Lawson <nlawson@salesforce.com>
2022-03-01 13:55:34 -0800

    repro connected callback timing

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 import { createElement } from "lwc";
 import App from "x/app";
 
-const elm = createElement("x-app", { is: App });
-document.body.appendChild(elm);
+class VanillaCustomElement extends HTMLElement {
+  connectedCallback() {
+    console.log('connected callback phase')
+  }
+}
+customElements.define('x-vanilla', VanillaCustomElement)
+
+
+function test(elm) {
+  const template = document.createElement('template');
+  console.log('appending to template content')
+  template.content.appendChild(elm);
+  console.log('appending template content to document.body')
+  document.body.appendChild(template.content);
+}
+
+console.log("--- lightning element ---")
+test(createElement("x-app", {is: App}))
+console.log("--- vanilla custom element ---")
+test(document.createElement('x-vanilla'))
--- src/modules/x/app/app.js
+++ src/modules/x/app/app.js
@@ -1,3 +1,7 @@
 import { LightningElement } from 'lwc';
 
-export default class App extends LightningElement {}
+export default class App extends LightningElement {
+  connectedCallback() {
+    console.log('connected callback phase')
+  }
+}
