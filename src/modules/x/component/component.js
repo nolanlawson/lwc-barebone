cba5929207d8c715464e8eae9357e6b3f5f8d57c
Nolan Lawson <nlawson@salesforce.com>
2022-03-17 15:00:51 -0700

    try directly importing css

--- src/modules/x/component/component.js
+++ src/modules/x/component/component.js
@@ -1,11 +1,12 @@
 import { LightningElement } from 'lwc';
-import native from './native.html'
-import synthetic from './synthetic.html'
+import native from './native.css'
+import synthetic from './synthetic.css'
 import template from './component.html'
 
 export default class Component extends LightningElement {
   render() {
-    const { stylesheets, stylesheetToken } = this.template.synthetic ? synthetic : native
+    const stylesheets = this.template.synthetic ? synthetic : native
+    const stylesheetToken = this.template.synthetic ? 'foobarbaz' : undefined
     Object.assign(template, { stylesheets, stylesheetToken })
     return template
   }
