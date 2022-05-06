937a117559ed15abdc1158ae07b02fef285b2771
Nolan Lawson <nlawson@salesforce.com>
2022-05-06 15:16:49 -0700

    decorator on non-lightning element

--- src/modules/x/app/app.js
+++ src/modules/x/app/app.js
@@ -1,3 +1,9 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+
+class NotAnLwcComponent {
+  @api foo = 'foo'
+}
+
+console.log(new NotAnLwcComponent()) // avoid tree-shaking it
 
 export default class App extends LightningElement {}
