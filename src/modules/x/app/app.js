3e0d3ce26fbfed0d76df60ea8f01c55d28dfea5a
Nolan Lawson <nlawson@salesforce.com>
2022-05-24 15:42:36 -0700

    bad button

--- src/modules/x/app/app.js
+++ src/modules/x/app/app.js
@@ -1,3 +1,5 @@
 import { LightningElement } from 'lwc';
 
-export default class extends LightningElement {}
+export default class extends LightningElement {
+  items = Array(100).fill().map((_, i) => i)
+}
