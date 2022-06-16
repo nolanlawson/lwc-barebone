c80ac4149e7cf1caf5483684fab6da3c6a795c14
Nolan Lawson <nlawson@salesforce.com>
2022-06-16 09:31:36 -0700

    static repro

--- src/modules/x/app/app.js
+++ src/modules/x/app/app.js
@@ -1,3 +1,9 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class extends LightningElement {}
+export default class extends LightningElement {
+    @track rows = [];
+
+    add() {
+        this.rows.push({ id: 1 });
+    }
+}
