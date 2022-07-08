c891b9e212bbac6533bafd0a66534eee0e7c2f82
Nolan Lawson <nlawson@salesforce.com>
2022-07-08 09:28:43 -0700

    demo of hydration with scoped css

--- src/modules/x/app/app.js
+++ src/modules/x/app/app.js
@@ -1,3 +1,7 @@
 import { LightningElement } from 'lwc';
 
-export default class extends LightningElement {}
+export default class extends LightningElement {
+  click() {
+    console.log('Hydration is working')
+  }
+}
