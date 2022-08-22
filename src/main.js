f47168ac8ed93bf18f0b2fecce622a205ea84e44
Nolan Lawson <nlawson@salesforce.com>
2022-08-22 15:38:36 -0700

    demo CE SSR

--- src/main.js
+++ src/main.js
@@ -2,3 +2,5 @@ import { hydrateComponent } from "lwc";
 import App from "x/app";
 
 hydrateComponent(document.querySelector('x-app'), App);
+
+console.log(`customElements.get('x-app') is:`, customElements.get('x-app'))
