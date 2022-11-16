03e1ce7abdf4cd8fc17ce646193df86547e95035
Nolan Lawson <nlawson@salesforce.com>
2022-11-16 09:31:08 -0800

    fix: repro third-party custom element

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createElement } from "lwc";
 import App from "x/app";
 
+customElements.define('third-party', class extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' }).innerHTML = '<div>I am a third-party custom element!</div>'
+  }
+
+})
+
 const elm = createElement("x-app", { is: App });
 document.body.appendChild(elm);
