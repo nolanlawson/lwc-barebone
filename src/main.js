2312109c91220a4999b8be4167fe398d4fa5bb43
Nolan Lawson <nlawson@salesforce.com>
2023-01-23 15:28:08 -0800

    fix: repro svg styling in older lwc version

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
+import '@lwc/synthetic-shadow'
 import { createElement } from "lwc";
-import App from "x/app";
+import App from "x/appLiCaTiOn";
 
 const elm = createElement("x-app", { is: App });
 document.body.appendChild(elm);
